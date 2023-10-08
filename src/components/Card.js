f5fc87bf002028c625231208ad03c033d56acc2e
amisha <amishaadwani0023@gmail.com>
2023-10-08 16:18:21 +0530

    minor changes made

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import Card from "react-bootstrap/Card";
 
 function Cards(prop) {
   return (
-    <Card onClick={prop.cardClick} style={{ width: "20rem" }}>
+    <Card onClick={prop.cardClick} style={{ width: "20rem" ,cursor:'pointer'}}>
       <Card.Img src={prop.src} style={{ height: "10rem", width: '15rem' }} />
       <Card.Body>
         <Card.Title>{prop.title}</Card.Title>
